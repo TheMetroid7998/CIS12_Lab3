c5fff0e01bccf7a64b09b015d89ca63829323044
Conrad Salazar <themetroid7998@gmail.com>
2024-09-10 15:06:49 -0700

    Successful implementation of input collection and confirmation.

--- CIS12_Lab3.py
+++ CIS12_Lab3.py
@@ -1,1 +1,19 @@
-print ("Hello World")
+def input_confirmation(conf):
+    conf = input("Is this correct? Y/N: ").strip().upper()
+    while conf != 'Y' and conf != 'N':
+        print("That's not a valid input. Please enter either 'Y' for yes or 'N' for no.")
+        conf = input("Is this correct? Y/N: ").strip().upper()
+    if conf == 'N':
+        get_input("OK. Please re-enter your desired text: ")
+    elif conf == 'Y':
+        print("Choice confirmed.")
+        return
+
+
+def get_input(message):
+    usr = input(f"{message}").strip().upper()
+    print(f"You entered {usr}.")
+    input_confirmation(usr)
+
+get_input("Enter Username: ")
+print("success!")
