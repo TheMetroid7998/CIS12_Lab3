e0a0e4f627ed5f2d9e559e06f4c6e1344c955f16
Conrad Salazar <themetroid7998@gmail.com>
2024-09-12 12:33:06 -0700

    Debugged code, cleaned up process. Redacted exit process.

--- CIS12_Lab3.py
+++ CIS12_Lab3.py
@@ -1,19 +1,18 @@
-import lab_chat as lc
+#import lab_chat as lc
 
-def input_confirmation(conf):
+def input_confirmation():
     conf = input("Is this correct? Y/N: ").strip().upper()
     while True:
         if conf == 'N':
-            get_input("OK. Please re-enter your desired text or press Enter to continue: ")
+            return False
         elif conf == 'Y':
             print("Choice confirmed.")
-            return
+            return True
         else: #conf != 'Y' and conf != 'N':
             print("That's not a valid input. Enter either 'Y' for yes or 'N' for no to confirm your choice.")
             conf = input("Is this correct? Y/N: ").strip().upper()
-            return conf
 
-def get_input(message):
+def get_username(message):
     usr = str(input(f"{message}").strip().upper())
     print(f"You entered {usr}.")
     return usr
@@ -26,26 +25,29 @@ def get_group(message):
 def get_message(message):
     msg = str(input(f"{message}").strip())
     if msg != 'exit_now':
-        print(f"{usr}: {msg}")
+        print(f"{usr}: {msg}") # would have used {usr}
         return msg
     elif msg == 'exit_now':
-        print("""
-You are about to leave this group. Are you sure?
-If this was done accidentally, input 'N', press Enter, and input 'Y' to return to the chat.
-""")
+        print("You are about to leave this group. Are you sure?")
+        while not input_confirmation():
+            print("Cancellation Confirmed. (But not really because I can't code.)")
+            return msg
     return msg
 
-# actual code starts here
+#def initialize_chat():
 
-get_input("Enter Username: ")
-input_confirmation(get_input)
+usr = get_username("Enter Username: ")
+while not input_confirmation():
+    usr = get_username("Enter Username: ")
 
-get_group("Enter the group you wish to join: ")
-input_confirmation(get_group)
+grp = get_group("Enter the group you wish to join: ")
+while not input_confirmation():
+    grp = get_group("Enter the group you wish to join: ")
+
 print(f"You have now entered group '{grp}'. Type anything to start chatting. Type 'exit_now' to leave the group.")
 
 while True:
-    get_message(">")
+    get_message(f">")
     continue
 
-print("success! the program has not crashed. semantic errors may still remain.")
+#print("success! the program has not crashed. semantic errors may still remain.")
